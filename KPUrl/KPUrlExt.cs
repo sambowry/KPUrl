224ba4b71e65484ec95d67823e4b2e025e1af10f
sambowry <sambowry@gmail.com>
2014-05-10 16:53:40 +0200

    event switch change
    The "-event" command line switch changed to pass the whole command line.
    Usage example:
    
    keepass.exe -event -arg1:value1 -arg2:value2

--- KPUrl/KPUrlExt.cs
+++ KPUrl/KPUrlExt.cs
@@ -7,8 +7,9 @@ using Microsoft.Win32;
 
 using KeePass.App.Configuration;
 
-using KeePass.Plugins;
+using KeePass.Util;
 using KeePass.Forms;
+using KeePass.Plugins;
 using KeePass.Resources;
 
 using KeePassLib;
@@ -16,52 +17,57 @@ using KeePassLib.Security;
 
 namespace KPUrl
 {
-  public sealed class KPUrlExt : Plugin
-  {
-    private IPluginHost m_host = null;
+	public sealed class KPUrlExt : Plugin
+	{
+		private IPluginHost m_host = null;
 
-    public override bool Initialize(IPluginHost host)
-    {
-      if (host == null) return false;
-      m_host = host;
+		public override bool Initialize(IPluginHost host)
+		{
+			if (host == null) return false;
+			m_host = host;
 
-      RegistryKey classes = Registry.CurrentUser.OpenSubKey(@"Software\Classes", true);
-      try
-      {
-        classes.DeleteSubKeyTree("KPUrl");
-      }
-      catch { }
+			RegistryKey classes = Registry.CurrentUser.OpenSubKey(@"Software\Classes", true);
+			try
+			{
+				classes.DeleteSubKeyTree("KPUrl");
+			}
+			catch { }
 
-      RegistryKey key = classes.CreateSubKey("KPUrl");
-      key.SetValue("", "URL:KPUrl protocol");
-      key.SetValue("URL Protocol", "");
+			RegistryKey key = classes.CreateSubKey("KPUrl");
+			key.SetValue("", "URL:KPUrl protocol");
+			key.SetValue("URL Protocol", "");
 
-      string exe = '"' + Application.ExecutablePath + '"';
-      key.CreateSubKey("DefaultIcon").SetValue("", exe + ",1");
-      key = key.CreateSubKey(@"shell\open");
-      key.SetValue("", "&Open");
+			string exe = '"' + Application.ExecutablePath + '"';
+			key.CreateSubKey("DefaultIcon").SetValue("", exe + ",1");
+			key = key.CreateSubKey(@"shell\open");
+			key.SetValue("", "&Open");
 
-      key.CreateSubKey(@"command").SetValue("", exe + " \"-event:%1\"");
+			key.CreateSubKey(@"command").SetValue("", exe + "-event \"-KPUrl:%1\"");
 
-      m_host.MainWindow.EventMsgReceived += OnEventMsgReceived;
+			m_host.MainWindow.EventMsgReceived += OnEventMsgReceived;
 
-      return true;
-    }
+			return true;
+		}
 
-    private void OnEventMsgReceived(object sender, EventMsgArg e)
-    {
-      MessageBox.Show("event string: '" + e.Arg + "'", "KPUrl", MessageBoxButtons.OK, MessageBoxIcon.Information);
-    }
+		private void OnEventMsgReceived(object sender, EventMsgArg e)
+		{
+			CommandLineArgs args = e.Args;
+			string s = "";
+			foreach(KeyValuePair<string,string> a in args.Parameters){
+				s += "'" + a.Key + "' = '" + a.Value + "'\r\n";
+			}
+			MessageBox.Show("event args:\r\n" + s, "KPUrl", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
 
-    public override void Terminate()
-    {
-      RegistryKey classes = Registry.CurrentUser.OpenSubKey(@"Software\Classes", true);
-      try
-      {
-        classes.DeleteSubKeyTree("KPUrl");
-      }
-      catch { }
-    }
+		public override void Terminate()
+		{
+			RegistryKey classes = Registry.CurrentUser.OpenSubKey(@"Software\Classes", true);
+			try
+			{
+				classes.DeleteSubKeyTree("KPUrl");
+			}
+			catch { }
+		}
 
-  }
+	}
 }

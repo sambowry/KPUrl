47aa9c2fb11aff847168c5a04acd2988f53be33a
sambowry <sambowry@gmail.com>
2014-05-10 21:19:21 +0200

    expose UrlSchemeOverrides
    Registering URP Protocols based on KeePass UrlSchemeOverrides.
    Existing protocol handlers will be saved at start and restored on exit.

--- KPUrl/KPUrlExt.cs
+++ KPUrl/KPUrlExt.cs
@@ -15,38 +15,40 @@ using KeePass.Resources;
 using KeePassLib;
 using KeePassLib.Security;
 
+using RenameRegistryKey;
+
 namespace KPUrl
 {
 	public sealed class KPUrlExt : Plugin
 	{
 		private IPluginHost m_host = null;
 
+		private RegistryUtilities ru = new RegistryUtilities();
+		private List<string> m_renamed = new List<string>();
+		private List<string> m_created = new List<string>();
+
 		public override bool Initialize(IPluginHost host)
 		{
 			if (host == null) return false;
 			m_host = host;
 
-			RegistryKey classes = Registry.CurrentUser.OpenSubKey(@"Software\Classes", true);
-			try
+			AppConfigEx config = KeePass.Program.Config;
+			foreach (AceUrlSchemeOverride url in config.Integration.UrlSchemeOverrides.BuiltInOverrides)
 			{
-				classes.DeleteSubKeyTree("KPUrl");
+				if (url.Enabled) RegisterProtocol(url.Scheme);
 			}
-			catch { }
-
-			RegistryKey key = classes.CreateSubKey("KPUrl");
-			key.SetValue("", "URL:KPUrl protocol");
-			key.SetValue("URL Protocol", "");
-
-			string exe = '"' + Application.ExecutablePath + '"';
-			key.CreateSubKey("DefaultIcon").SetValue("", exe + ",1");
-			key = key.CreateSubKey(@"shell\open");
-			key.SetValue("", "&Open");
-
-			key.CreateSubKey(@"command").SetValue("", exe + " -event \"-KPUrl:%1\"");
+			foreach (AceUrlSchemeOverride url in config.Integration.UrlSchemeOverrides.CustomOverrides)
+			{
+				if (url.Enabled) RegisterProtocol(url.Scheme);
+			}
 
 			m_host.MainWindow.EventMsgReceived += OnEventMsgReceived;
-
 			return true;
+		}
+
+		public override void Terminate()
+		{
+			DeRegisterAll();
 		}
 
 		private void OnEventMsgReceived(object sender, EventMsgArg e)
@@ -59,14 +61,65 @@ namespace KPUrl
 			MessageBox.Show("event args:\r\n" + s, "KPUrl", MessageBoxButtons.OK, MessageBoxIcon.Information);
 		}
 
-		public override void Terminate()
+		private void RegisterProtocol(string protocol)
 		{
 			RegistryKey classes = Registry.CurrentUser.OpenSubKey(@"Software\Classes", true);
+			string exe = '"' + Application.ExecutablePath + '"';
 			try
 			{
-				classes.DeleteSubKeyTree("KPUrl");
+				RegistryKey subkey = classes.OpenSubKey(protocol,true);
+				if (subkey != null)
+				{
+					subkey.Close();
+					m_renamed.Add(protocol);
+					ru.RenameSubKey(classes, protocol, "KPUrl-" + protocol);
+				}
+
+				m_created.Add(protocol);
+				RegistryKey key = classes.CreateSubKey(protocol);
+				key.SetValue("", "URL:" + protocol + " protocol");
+				key.SetValue("URL Protocol", "");
+
+				key = key.CreateSubKey(@"shell\open");
+				key.SetValue("", "&Open");
+
+				key.CreateSubKey(@"command").SetValue("", exe+" -event \"-KPUrl:%1\"");
 			}
-			catch { }
+			catch(Exception e)
+			{
+				System.Windows.Forms.MessageBox.Show(e.Message, "KPUrl: RegisterProtocol('"+protocol+"')");
+			}
+			classes.Close();
+		}
+
+		private void DeRegisterAll()
+		{
+			RegistryKey classes = Registry.CurrentUser.OpenSubKey(@"Software\Classes", true);
+			foreach (string protocol in m_created)
+			{
+				try
+				{
+					classes.DeleteSubKeyTree(protocol);
+				}
+				catch(Exception e)
+				{
+					System.Windows.Forms.MessageBox.Show(e.Message, "KPUrl: DeleteSubKey('" + protocol + "')");
+				}
+			}
+
+			foreach (string protocol in m_renamed)
+			{
+				try
+				{
+					ru.RenameSubKey(classes, "KPUrl-" + protocol, protocol);
+				}
+				catch(Exception e)
+				{
+					System.Windows.Forms.MessageBox.Show(e.Message, "KPUrl: RenameSubKey('KPUrl-" + protocol + "', '" + protocol +"')");
+				}
+			}
+
+			classes.Close();
 		}
 
 	}

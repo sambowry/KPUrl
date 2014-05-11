d31851418634e80ce76db69c486ef31fea2777a1
sambowry <sambowry@gmail.com>
2014-05-11 19:56:46 +0200

    small changes
    A KeePass entry is matching to the received url if the entry title
    equals to the url "DnsSafeHost" and the scheme in the entry URL field
    match to the scheme of the received url.

--- KPUrl/KPUrlExt.cs
+++ KPUrl/KPUrlExt.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Diagnostics;
 using System.Windows.Forms;
+using System.Collections.Generic;
 using Microsoft.Win32;
 
+using KeePass.App;
 using KeePass.App.Configuration;
-
+using KeePass.UI;
 using KeePass.Util;
 using KeePass.Forms;
 using KeePass.Plugins;
@@ -14,6 +16,7 @@ using KeePass.Resources;
 
 using KeePassLib;
 using KeePassLib.Security;
+using KeePassLib.Collections;
 
 using RenameRegistryKey;
 
@@ -51,28 +54,115 @@ namespace KPUrl
 			DeRegisterAll();
 		}
 
+		private void ShowEventArgs(CommandLineArgs args)
+		{
+			string s = "";
+			s += "FileName: '" + args.FileName + "'\r\n";
+			s += "FileNames:\r\n";
+			int i = 1;
+			foreach (string f in args.FileNames)
+			{
+				s += "  [" + i++ + "]: '" + f + "'\r\n";
+			}
+			s += "Parameters:\r\n";
+			foreach (KeyValuePair<string, string> a in args.Parameters)
+			{
+				s += "  '" + a.Key + "' = '" + a.Value + "'\r\n";
+			}
+			Uri uri = new Uri(args.FileName);
+			s += "\r\nURL:\r\n";
+			s += "  OriginalString: '" + uri.OriginalString + "'\r\n";
+			s += "  Scheme:         '" + uri.Scheme + "'\r\n";
+			s += "  DnsSafeHost:    '" + uri.DnsSafeHost + "'\r\n";
+
+			MessageBox.Show(s, "KPUrl, ShowEventArgs()", MessageBoxButtons.OK, MessageBoxIcon.Information );
+		}
+
+		private DialogResult ShowEntry(PwDatabase db, PwEntry pe)
+		{
+			PwEntryForm pForm = new PwEntryForm();
+			pForm.InitEx(pe, PwEditMode.ViewReadOnlyEntry, db, m_host.MainWindow.ClientIcons, false, false);
+			DialogResult result = pForm.ShowDialog();
+			UIUtil.DestroyForm(pForm);
+			return result;
+		}
+
+		private bool MatchEntry( PwEntry pe, Uri uri)
+		{
+			if (String.Equals(pe.Strings.ReadSafe("Title"), uri.DnsSafeHost, StringComparison.OrdinalIgnoreCase))
+			{
+				try
+				{
+					Uri peUri = new Uri(pe.Strings.ReadSafe("URL"));
+					return String.Equals(peUri.Scheme, uri.Scheme, StringComparison.OrdinalIgnoreCase);
+				}
+				catch { }
+			}
+			return false;
+		}
+
+		private PwEntry FindEntry(string url, bool show = false)
+		{
+			Uri uri = new Uri(url);
+			var docs = m_host.MainWindow.DocumentManager.Documents;
+			PwEntry found = null;
+			foreach( PwDocument d in docs)
+			{
+				SearchParameters sp = new SearchParameters();
+				PwObjectList<PwEntry> result = new PwObjectList<PwEntry>();
+				d.Database.RootGroup.SearchEntries(sp, result);
+				foreach(PwEntry entry in result)
+				{
+					if (MatchEntry(entry,uri))
+					{
+						if (show) ShowEntry(d.Database, entry);
+						found = entry;
+						break;
+					}
+				}
+				if( found != null ) break;
+			}
+			return found;
+		}
+
 		private void OnEventMsgReceived(object sender, EventMsgArg e)
 		{
 			CommandLineArgs args = e.Args;
-			string s = "";
-			foreach(KeyValuePair<string,string> a in args.Parameters){
-				s += "'" + a.Key + "' = '" + a.Value + "'\r\n";
+			if (String.Equals(args[AppDefs.CommandLineOptions.EventMsg], "KPUrl", StringComparison.OrdinalIgnoreCase))
+			{
+				ShowEventArgs(args);
+				PwEntry entry = FindEntry(args.FileName, true);
+				if (entry != null)
+				{
+					KeePass.Util.WinUtil.OpenEntryUrl(entry);
+				}
+				else
+				{
+					MessageBox.Show("No entry found for URL='" + args.FileName + "'",
+						"KPUrl, OnEventMsgReceived()", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
 			}
-			MessageBox.Show("event args:\r\n" + s, "KPUrl", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+
+		private bool IsRegistered( RegistryKey parentKey, string protocol)
+		{
+			RegistryKey key = parentKey.OpenSubKey(protocol);
+			return (key != null);
 		}
 
 		private void RegisterProtocol(string protocol)
 		{
-			RegistryKey classes = Registry.CurrentUser.OpenSubKey(@"Software\Classes", true);
 			string exe = '"' + Application.ExecutablePath + '"';
+			RegistryKey classes = Registry.CurrentUser.OpenSubKey(@"Software\Classes", true);
 			try
 			{
-				RegistryKey subkey = classes.OpenSubKey(protocol,true);
-				if (subkey != null)
+				if (IsRegistered(classes,protocol))
 				{
-					subkey.Close();
 					m_renamed.Add(protocol);
-					ru.RenameSubKey(classes, protocol, "KPUrl-" + protocol);
+					if ( ! IsRegistered(classes,"KPUrl-" + protocol))
+					{
+						ru.RenameSubKey(classes, protocol, "KPUrl-" + protocol);
+					}
 				}
 
 				m_created.Add(protocol);
@@ -83,7 +173,7 @@ namespace KPUrl
 				key = key.CreateSubKey(@"shell\open");
 				key.SetValue("", "&Open");
 
-				key.CreateSubKey(@"command").SetValue("", exe+" -event \"-KPUrl:%1\"");
+				key.CreateSubKey(@"command").SetValue("", exe+" -event:KPUrl \"%1\"");
 			}
 			catch(Exception e)
 			{
